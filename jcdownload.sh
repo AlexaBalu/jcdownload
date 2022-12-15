43eb09ae668caaca8aa2a4233f8e289101ee7c8b
alexabalu <57348956+AlexaBalu@users.noreply.github.com>
2022-12-15 21:23:52 +0100

    Fixed bash script

--- jcdownload.sh
+++ jcdownload.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Please replace PUT_HERE_COMMON_KEY with key found over the Internet (use phrases like Wii U Common Key)
+commonKey=PUT_HERE_COMMON_KEY
+
+if [[ $commonKey == "PUT_HERE_COMMON_KEY" ]]
+then
+	echo "You have not set common key. Open this script and edit one value."
+	exit 1
+fi
+
 if [[ -z $1 ]]
 then
 	echo Title Key or folder of resume: 
@@ -8,7 +17,7 @@ else
    titleKey=$1
 fi
 
-java -jar jcdownload.jar d7b00402659ba2abd2cb0db27fa2b656 "$titleKey"
+java -jar jcdownload.jar $commonKey "$titleKey"
 
 
 read -p "Press [Enter] key to exit..."
